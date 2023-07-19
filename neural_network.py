8fb243d85e98e38a955cba69cfdea3254d390eb1
hasanaliozkan-dev <hasanaliozkan@posta.mu.edu.tr>
2023-07-19 09:51:57 +0300

    prediction methods added

--- neural_network.py
+++ neural_network.py
@@ -1112,12 +1112,39 @@ class Model():
         with open(path,'wb') as f:
             pickle.dump(model,f)
     
+    def predict(self, X , * , batch_size= None):
+        prediction_steps = 1
+
+        if batch_size is not None:
+            prediction_steps = len(X) //batch_size
+
+            if prediction_steps*batch_size < len(X):
+                prediction_steps +=1
+
+        output = []
+
+        for step in range(prediction_steps):
+            if batch_size is None:
+                batch_X = X 
+
+            else:
+                batch_X = X[step*batch_size:(step+1)*batch_size] 
+
+                batch_output = self.forward(batch_X,training=False)
+
+                output.append(batch_output)
+
+        return np.vstack(output)
+                   
+
 
     @staticmethod
     def load(path):
         with open(path,'rb') as f:
             model = pickle.load(f)
         return model
+
+    
 class Accuracy():
 
     class Main():
